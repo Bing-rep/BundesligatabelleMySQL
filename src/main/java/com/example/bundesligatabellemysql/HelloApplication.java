25d7d1ff277aa31ba26c974166e321d119cd8ad4
Rexonfury <nathal15@gmx.de>
2023-06-29 21:13:47 +0200

    größe

--- src/main/java/com/example/bundesligatabellemysql/HelloApplication.java
+++ src/main/java/com/example/bundesligatabellemysql/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Bundesligatabelle.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 850, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("Bundesligatabelle");
         stage.setScene(scene);
         stage.show();
     }
